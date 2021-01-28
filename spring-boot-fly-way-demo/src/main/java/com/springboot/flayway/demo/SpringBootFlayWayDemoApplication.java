c791a347c206f6e0d924fa2730bde97f4e6afc68
Sumit <sumitgupta28@gmail.com>
2021-01-28 03:42:27 -0500

    Init

---
+++ spring-boot-fly-way-demo/src/main/java/com/springboot/flayway/demo/SpringBootFlayWayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.flayway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlayWayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlayWayDemoApplication.class, args);
+	}
+
+}
